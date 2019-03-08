2cff516ae1e399d8b82db746d6e9d482835b1170
Vaios <tsegkos@mail.com>
2019-03-08 16:27:18 +0200

    Ready

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,14 +1,10 @@
 import React from "react";
 
-class Titles extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Weather Finder</h1>
-                <p>Find out temperature, conditions and more...</p>
-            </div>
-        );
-    }
-};
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">Weather App</h1>
+		<h3 className="title-container__subtitle">Μάθε τη θερμοκρασία, τις συνθήκες και άλλα ...</h3>
+	</div>
+);
 
 export default Titles;
